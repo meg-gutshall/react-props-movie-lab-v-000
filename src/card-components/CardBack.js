c0e8c4ccaa3477b66fcc2420e4956345abfd86ba
Meg Gutshall <meghan.gutshall@gmail.com>
2021-03-05 15:41:41 -0500

    commit tree ddd963dfc60092545fe2df797cee807f53a4a8cb

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -11,11 +11,7 @@ const imgMapper = {0: zero, 1: one, 2: two, 3: three, 4: four, 5: five}
 export default class CardBack extends Component {
   
   generateRatingElement = () => {
-    if (this.props.IMDBRating == null) {
-      return <h4>No Rating Found</h4>
-    } else {
-      return <img src={imgMapper[this.props.IMDBRating]} alt="Movie rating" />
-    }
+    return (this.props.IMDBRating == null) ? <h4>No Rating Found</h4> : <img src={imgMapper[this.props.IMDBRating]} alt="Movie rating" />
   }
   
   render() {
